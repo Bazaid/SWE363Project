2f396c34c5904cdbfba406adeb32eb4d3e3dad26
Abdullah Bazaid <bazaid.abdullah@me.com>
2019-04-16 16:12:08 +0300

    update postreq

--- include/config.php
+++ include/config.php
@@ -1,5 +1,6 @@
 <?php
-  $mysqli =  mysqli_connect("127.0.0.1:32810","root","","project");
+  header("Access-Control-Allow-Origin: *");
+  $mysqli =  mysqli_connect("127.0.0.1:32811","root","","project");
   define('__URL', 'http://localhost:8000/');
   if (!$mysqli) {
    die('Could not connect: ' . mysql_error());
--- postRequest.php
+++ postRequest.php
@@ -4,12 +4,14 @@ require_once('include/template.php');
 require_once('include/session.php');
 
 if (!Session::isLoggedIn()) {
+    echo 'no';
     http_response_code(400);
     exit();
 }
 
 
 if (!isset($_POST['service_id']) || !isset($_POST['info']) || empty($_POST['info'])) {
+    var_dump($_POST);
     http_response_code(400);
     exit();
 }
@@ -18,17 +20,17 @@ $info = $_POST['info'];
 $service_id = $_POST['service_id'];
 $user = Session::get("user");
 $date = date('Y-m-d');
-$sql = "INSERT INTO `requests` 
-(`service_id`, `user_id`, `staff_id`, `info`, `completed`, `created_at`, `completed_at`, `building`, `room` ) 
+$sql = "INSERT INTO `requests` (`service_id`, `user_id`, `staff_id`, `info`, `completed`, `created_at`, `completed_at`, `room`, `building`)
 VALUES
-($service_id, $user->id, NULL, '$info', 0, '$date', NULL, $user->building, $user->room);;";
-
+	('$service_id', '$user->id', -1, '$info', 0, '$date', NULL, $user->room, $user->building);
+";
 if ($result = $mysqli->query($sql)) {
 
     $result->close();
     $mysqli->close();
     exit('done');
 }else {
+
     $mysqli->close();
     http_response_code(400);
     exit(); 
